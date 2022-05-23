2495ca1a7a86c2e369586ed888a46c731a095cd9
Sophia de Lisieux Zocoler <sophiazocoler@gmail.com>
2022-05-23 18:41:35 -0300

    feat: add tests

--- houses-of-marabahan-back/src/controllers/houseController.ts
+++ houses-of-marabahan-back/src/controllers/houseController.ts
@@ -5,6 +5,7 @@ import * as houseService from '../services/houseService.js';
 export async function createHouse(req: Request, res: Response) {
   const { houseData } = req.body;
   const starterItem = req.body.starterItem;
+
   console.log(starterItem);
 
   await houseService.signUp(houseData, starterItem);
