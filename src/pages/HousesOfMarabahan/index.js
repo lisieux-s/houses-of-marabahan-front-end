8a29515ab5408f0fdfa836606aa492a39463091d
Sophia de Lisieux Zocoler <sophiazocoler@gmail.com>
2022-05-31 17:10:29 -0300

    feat: basic instructions on /

--- src/pages/HousesOfMarabahan/index.js
+++ src/pages/HousesOfMarabahan/index.js
@@ -1,17 +1,21 @@
 import { useNavigate } from 'react-router-dom';
 
-import useAuth from '../../hooks/useAuth'
+import useAuth from '../../hooks/useAuth';
 
-import { StyledLink } from '../../components/StyledLink/style'
+import { StyledLink } from '../../components/StyledLink/style';
 
 export default function HousesOfMarabahan() {
-    const navigate = useNavigate();
-    const { token } = useAuth();
+  const navigate = useNavigate();
+  const { token } = useAuth();
 
-    return(
-        <main>
-            Welcome to Marabahan!
-            <StyledLink to='items'><button>Edit items</button></StyledLink>
-        </main>
-    )
+  return (
+    <main>
+      <h2>Welcome to Marabahan!</h2>
+      <p>Found your house, have characters join and collect items.</p>
+      <p>Start by clicking "ENTER" and creating your house and your first character.</p>
+      {
+        //<StyledLink to='items'><button>Edit items</button></StyledLink>
+      }
+    </main>
+  );
 }
